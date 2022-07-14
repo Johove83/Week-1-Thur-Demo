6ec9270aecc0fb216aab2b3e139208e1d0edf6ec
unknown <johnathan.overton83@gmail.com>
2022-07-14 12:11:47 -0600

    More inputs.

---
+++ Week-1-Thur-Demo-test0630/TestFolder/inputs-02.py
@@ -0,0 +1,19 @@
+name = input("Welcome to the Boba Shop! What is your name?\n\n")
+
+if name != "":
+    print("\n\nHello, " + name + ".\n\n")
+    beverage = input("\n\nWhat kind of boba drink would you like?\n\n")
+    sweetness_level = input("\n\nHow sweet do you want your drink: 0, 50, 100, or 200?\n\n")
+    if sweetness_level == 50:
+        sweetness = "half sweetened"
+    elif sweetness_level == 100:
+        sweetness = "normal sweet"
+    elif sweetness_level == 200:
+        sweetness = "super sweet"
+    else:
+        sweetness = "non-sweet"
+    print(f"Your order of {beverage} boba with a sweet level of {sweetness} will be out shortly.")
+else:
+    print("You didn't give us your name! Goodbye.")
+
+    
