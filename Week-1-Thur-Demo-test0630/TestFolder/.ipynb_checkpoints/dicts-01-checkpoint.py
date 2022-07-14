71d3f208b450fb163b902c330868601ffe51608a
unknown <johnathan.overton83@gmail.com>
2022-07-14 13:36:08 -0600

    Build a manual dictionary so I can more appreciate prebuilt dictionaries.

---
+++ Week-1-Thur-Demo-test0630/TestFolder/.ipynb_checkpoints/dicts-01-checkpoint.py
@@ -0,0 +1,25 @@
+san_francisco = {}
+san_francisco["west_coast"] = True
+san_francisco["has_multiple_bridges"] = True
+san_francisco["known_for_pizza"] = False
+san_francisco["coastal"] = True
+san_francisco["snows"] = False
+san_francisco["very hot"] = False
+san_francisco["mayor"] = "London Breed"
+san_francisco["state"] = "California"
+san_francisco["country"] = "USA"
+san_francisco["best_food"] = "burritos"
+san_francisco["sports_teams"] = ["Giants", "Warriors", "49ers"]
+san_francisco["tallest_building"] = "SalesForce Building"
+san_francisco["population"] = 884364
+san_francisco["city_size"] = "large"
+san_francisco["median_house_price"] = 1610000
+san_francisco["famous_residents"] = ["Maya Angelou", "Robert Frost", "Carlos Santana"]
+san_francisco["homeless_pop"] = 1150
+san_francisco["political_leaning"] = "Democrat"
+san_francisco["notable_attractions"] = ["Alcatraz", "Golden Gate Bridge", "Fisherman's Wharf"]
+san_francisco["natural_disasters"] = ["Earthquakes"]
+
+print(san_francisco)
+
+#built it a step at a time because repetition will make me love the update method?
