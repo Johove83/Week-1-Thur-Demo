21cf70da07618dd271e0a27a4688d46e84533e90
unknown <johnathan.overton83@gmail.com>
2022-07-14 13:07:07 -0600

    It gets easier as soon as you convert integers into strings where strings are needed.

--- Week-1-Thur-Demo-test0630/TestFolder/.ipynb_checkpoints/inputs-02-checkpoint.py
+++ Week-1-Thur-Demo-test0630/TestFolder/.ipynb_checkpoints/inputs-02-checkpoint.py
@@ -1,19 +1,21 @@
 name = input("Welcome to the Boba Shop! What is your name?\n\n")
-
+sweetness = ""
 if name != "":
     print("\n\nHello, " + name + ".\n\n")
     beverage = input("\n\nWhat kind of boba drink would you like?\n\n")
     sweetness_level = input("\n\nHow sweet do you want your drink: 0, 50, 100, or 200?\n\n")
-    if sweetness_level == 50:
+    if sweetness_level == str(50):
         sweetness = "half sweetened"
-    elif sweetness_level == 100:
+    elif sweetness_level == str(100):
         sweetness = "normal sweet"
-    elif sweetness_level == 200:
+    elif sweetness_level == str(200):
         sweetness = "super sweet"
-    else:
-        sweetness = "non-sweet"
+    else:    
+        sweetness = "non-sweet"     
     print(f"Your order of {beverage} boba with a sweet level of {sweetness} will be out shortly.")
 else:
     print("You didn't give us your name! Goodbye.")
-
     
+    
+
+#issues until I realized I must convert integers into strings.
